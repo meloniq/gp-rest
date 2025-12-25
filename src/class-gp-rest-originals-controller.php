35d0c6a8618e628082bf9abc4ce4b08f2f82e783
meloniq <meloniq@gmail.com>
2025-12-25 17:26:29 +0100

    add incomplete originals import

--- src/class-gp-rest-originals-controller.php
+++ src/class-gp-rest-originals-controller.php
@@ -58,6 +58,20 @@ class GP_REST_Originals_Controller extends GP_REST_Controller {
 			)
 		);
 
+		// POST originals/import .
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/import',
+			array(
+				array(
+					'methods'             => WP_REST_Server::CREATABLE,
+					'callback'            => array( $this, 'import_originals' ),
+					'permission_callback' => array( $this, 'import_originals_permissions_check' ),
+					'args'                => $this->get_collection_params(),
+				),
+			)
+		);
+
 		// GET originals/{id} .
 		register_rest_route(
 			$this->namespace,
@@ -109,6 +123,87 @@ class GP_REST_Originals_Controller extends GP_REST_Controller {
 		}
 
 		$response = rest_ensure_response( $data );
+
+		return $response;
+	}
+
+	/**
+	 * Handles POST requests to /originals/import endpoint.
+	 *
+	 * @param WP_REST_Request $request The REST request.
+	 *
+	 * @return WP_REST_Response The REST response.
+	 */
+	public function import_originals( $request ) {
+		global $wp_filesystem;
+
+		// Load the WordPress file system.
+		include_once ABSPATH . 'wp-admin/includes/file.php';
+
+		$project_id = absint( $request->get_param( 'project_id' ) );
+		if ( ! $project_id ) {
+			return $this->response_404_project_not_found();
+		}
+
+		$project = GP::$project->get( $project_id );
+		if ( ! $project ) {
+			return $this->response_404_project_not_found();
+		}
+
+		// file format.
+		$format = $request->get_param( 'format' );
+		if ( ! in_array( $format, array( 'auto', 'android', 'po', 'mo', 'resx', 'strings', 'properties', 'json', 'jed1x', 'ngx' ), true ) ) {
+			return new WP_REST_Response(
+				array(
+					'code'    => 'invalid_format',
+					'message' => __( 'The format parameter.', 'gp-rest' ),
+				),
+				400
+			);
+		}
+
+		$import_file = $request->get_param( 'import-file' );
+		// save content into a temp file.
+		WP_Filesystem();
+		$filename = wp_tempnam();
+		$success  = $wp_filesystem->put_contents( $filename, $import_file );
+		if ( ! $success ) {
+			return new WP_REST_Response(
+				array(
+					'code'    => 'file_write_error',
+					'message' => __( 'Failed to write the uploaded file.', 'gp-rest' ),
+				),
+				500
+			);
+		}
+
+		// TODO: Fix this part.
+		$format_obj = gp_get_import_file_format( $format, $filename );
+		if ( ! $format_obj ) {
+			return new WP_REST_Response(
+				array(
+					'code'    => 'invalid_file_format',
+					'message' => __( 'The imported file format is invalid or not supported.', 'gp-rest' ),
+				),
+				400
+			);
+		}
+
+		$translations = $format_obj->read_originals_from_file( $filename );
+
+		$imported_count = GP::$original->import_from_project( $project, $translations );
+
+		list( $originals_added, $originals_existing, $originals_fuzzied, $originals_obsoleted, $originals_error ) = $imported_count;
+
+		$data = array(
+			'originals_added'     => $originals_added,
+			'originals_existing'  => $originals_existing,
+			'originals_fuzzied'   => $originals_fuzzied,
+			'originals_obsoleted' => $originals_obsoleted,
+			'originals_error'     => $originals_error,
+		);
+
+		$response = new WP_REST_Response( $data, 200 );
 
 		return $response;
 	}
@@ -169,6 +264,19 @@ class GP_REST_Originals_Controller extends GP_REST_Controller {
 	 */
 	public function get_originals_permissions_check( $request ) {
 		return true;
+	}
+
+	/**
+	 * Permission check for importing a new originals.
+	 *
+	 * @param WP_REST_Request $request The REST request.
+	 *
+	 * @return bool True if the request has permission, false otherwise.
+	 */
+	public function import_originals_permissions_check( $request ) {
+		$project_id = absint( $request->get_param( 'project_id' ) );
+
+		return $this->current_user_can( 'write', 'project', $project_id );
 	}
 
 	/**
